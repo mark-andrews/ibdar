a7d72657eeb9c663b12620585be6219025fac6cb
Mark Andrews <mjandrews.org@gmail.com>
2025-06-10 18:20:48 +0100

    Update script and blackboard

--- scripts/script1.R
+++ scripts/script1.R
@@ -32,6 +32,20 @@ bernoulli_posterior_plot(n, m, alpha = 1, beta = 1)
 bernoulli_posterior_plot(n, m, alpha = 1, beta = 1) + xlim(0.4, 0.75)
 bernoulli_posterior_plot(n, m, alpha = 3, beta = 5) + xlim(0.4, 0.75)
 
+# posterior summary with flat prior
+bernoulli_posterior_summary(n, m, alpha =1, beta = 1)
+# posterior summary with 3, 5
+bernoulli_posterior_summary(n, m, alpha =3, beta = 5)
 
+get_beta_hpd(m + 1, n - m + 1) # HPD for the uniform prior
+# quantile distribution
+qbeta(0.025, m + 1, n - m + 1)
+qbeta(0.975, m + 1, n - m + 1)
 
-
+# sample 1 million samples from a N(100, 15^2)
+set.seed(41)
+x <- rnorm(n = 1e6, mean = 100, sd = 15)
+mean(x)
+sd(x)
+pnorm(130, mean = 100, sd = 15)
+mean(x <= 130)
