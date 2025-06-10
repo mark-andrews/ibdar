cf78a24729a75510a60ee55eb7441108e1290e14
Mark Andrews <mjandrews.org@gmail.com>
2025-06-10 15:26:46 +0100

    Add script1.R and blackboard

---
+++ scripts/script1.R
@@ -0,0 +1,34 @@
+#source("https://raw.githubusercontent.com/mark-andrews/ibdar/refs/heads/ibdar25/priorexposure/R/bernoulli_models.R")
+library(priorexposure)
+library(tidyverse)
+
+m <- 139
+n <- 250
+bernoulli_likelihood(n, m)
+
+m <- 14
+n <- 25
+bernoulli_likelihood(n, m)
+
+# plot different beta priors
+beta_plot(alpha = 3, beta = 5)
+beta_plot(alpha = 5, beta = 3)
+beta_plot(alpha = 9, beta = 15)
+beta_plot(alpha = 1, beta = 1)
+
+# alpha = 3, and beta = 5
+# the posterior is this ....
+n <- 250
+m <- 139
+alpha <- 3
+beta <- 5
+beta_plot(m + alpha, n - m + beta)
+bernoulli_posterior_plot(n, m, alpha = alpha, beta = beta)
+bernoulli_posterior_plot(n, m, alpha, beta)
+bernoulli_posterior_plot(n, m, beta = beta, alpha =  alpha)
+# change the prior to a uniform prior
+bernoulli_posterior_plot(n, m, alpha = 1, beta = 1)
+
+
+
+
